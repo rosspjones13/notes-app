136e463f73e5a71947727d4dab36a2b6ed0e663f
Ross Jones <rossmosis.jones@gmail.com>
2019-06-14 19:20:53 -0400

    created add note function, save note function and load note function'

--- notes.js
+++ notes.js
@@ -1,9 +1,38 @@
+const fs = require('fs')
+
 const getNotes = function () {
   return "Your notes..."
 }
 
 const addNote = function (title, body) {
+  const notes = loadNotes()
+  const duplicateNotes = notes.filter(note => note.title === title)
+  
+  if (duplicateNotes.length === 0) {
+    notes.push({
+      title,
+      body
+    })
+    saveNotes(notes)
+    console.log('New note added!')
+  } else {
+    console.log('Note title taken!')
+  }
 
+}
+
+const saveNotes = function (notes) {
+  const dataJSON = JSON.stringify(notes)
+  fs.writeFileSync('notes.json', dataJSON)
+}
+
+const loadNotes = function () {
+  try {
+    const dataBuffer = fs.readFileSync('notes.json')
+    return JSON.parse(dataBuffer)
+  } catch (e) {
+    return []
+  }
 }
 
 module.exports = {
