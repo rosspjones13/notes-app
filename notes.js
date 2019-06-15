cb9dc70d41e538ae24a60d2ec7efc79cb9b5113a
Ross Jones <rossmosis.jones@gmail.com>
2019-06-14 22:13:13 -0400

    added remove command, remove note function and chalk styling to logs

--- notes.js
+++ notes.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const chalk = require('chalk')
 
 const getNotes = function () {
   return "Your notes..."
@@ -14,11 +15,22 @@ const addNote = function (title, body) {
       body
     })
     saveNotes(notes)
-    console.log('New note added!')
+    console.log(chalk.green.inverse('New note added!'))
   } else {
-    console.log('Note title taken!')
+    console.log(chalk.red.inverse('Note title taken!'))
   }
+}
 
+const removeNote = function (title) {
+  const notes = loadNotes()
+  const filteredNotes = notes.filter(note => note.title !== title)
+
+  if (filteredNotes.length === notes.length) {
+    console.log(chalk.red.inverse('No notes with that title to remove!'))
+  } else {
+    console.log(chalk.green.inverse('Removed note with the title: ' + title))
+    saveNotes(filteredNotes)
+  }
 }
 
 const saveNotes = function (notes) {
@@ -37,5 +49,6 @@ const loadNotes = function () {
 
 module.exports = {
   getNotes,
-  addNote
+  addNote,
+  removeNote
 }
